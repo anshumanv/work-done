a46f47f184e3f6e40f058eab14a740cb1497db97
Anshuman Verma <anshu.av97@gmail.com>
2019-01-17 20:04:04 +0530

    Add loaders in profile, show register page when user not found

--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react'
+import { Dimmer, Loader, Image, Segment } from 'semantic-ui-react'
 import UserCard from './UserCard'
+import UserForm from './UserForm'
 
 export default class Profile extends Component {
   
   state = {
-    user: null
+    user: null,
+    loading: true,
+    registeredUser: null,
   }
 
   componentDidMount() {
@@ -13,19 +17,37 @@ export default class Profile extends Component {
 
     contract.methods.users(drizzleState.accounts[0]).call().then(res => {
       const { userName, userAddress, email, info } = res;
+      console.log(res)
+      if (!res.email) {
+        return this.setState({
+          loading: false,
+          registeredUser: false,
+        })
+      }
       this.setState({
         user: {
           userName,
           userAddress,
           email,
           info,
-        }
+        }, loading: false, registeredUser: true
       })
     })
   }
   
   render() {
-    const { user } = this.state;
+    const { user, loading, registeredUser } = this.state;
+    const { drizzle, drizzleState } = this.props;
+
+    if (loading) return (
+      <Dimmer active>
+        <Loader>Loading</Loader>
+      </Dimmer>
+    )
+
+    if (!registeredUser) return (
+      <UserForm drizzle={drizzle} drizzleState={drizzleState} />
+    )
     return (
       <div>
         <UserCard user={user} />
