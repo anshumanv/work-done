162a35254d5219cc4f0725215f1a2902170024bf
Anshuman Verma <anshu.av97@gmail.com>
2019-01-17 10:24:48 +0530

    Add landing template

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import ReadString from './ReadString';
 import SetString from './SetString';
+import Landing from './Landing';
 
 
 class App extends Component {
@@ -36,7 +37,8 @@ class App extends Component {
       <div className="App">
         <Router>
           <div>
-            <Route path="/" exact render={() =>  <ReadString drizzle={drizzle} drizzleState={drizzleState} />} />
+            <Route path="/" exact render={() =>  <Landing />} />
+            <Route path="/read" exact render={() =>  <ReadString drizzle={drizzle} drizzleState={drizzleState} />} />
             <Route path="/edit" exact render={() => <SetString drizzle={drizzle} drizzleState={drizzleState} />} />
           </div>
         </Router>
