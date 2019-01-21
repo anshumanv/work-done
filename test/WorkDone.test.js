12d120e4d739722abaf47645dcd003a11b9143fa
Anshuman Verma <anshu.av97@gmail.com>
2019-01-21 11:59:52 +0530

    Add test for updating user profile

--- test/WorkDone.test.js
+++ test/WorkDone.test.js
@@ -29,5 +29,34 @@ contract('WorkDone', accounts => {
     assert.equal(info, 'I watch anime', 'Info of the created user is incorrect')
     assert.equal(0, donationsRecieved, 'Donation recieved by the created user is incorrect')
     assert.equal(0, donationsGiven, 'Donation given by the created user is incorrect')
-  })
+  });
+
+  // A test to donate some amount to a user
+  it('should update the profile of a user', async () => {
+    // Instance of the deployed contract
+    const workDone = await WorkDone.deployed()
+    
+    // Create a user to be updated later
+    await workDone.createUser('user1', 'I watch anime', 'user1@user1.com', {from: user1});
+
+    // Call the update profile function to update the info of the user
+    await workDone.updateProfile('natsu', 'I read manga', 'anime@cool.com', { from: user1 })
+
+    // Get the updated user
+    const userData = await workDone.users.call(user1)
+    
+    // console.log(userData)
+    
+    // Destructure the details of the updated user
+    const { userName, info, userAddress, email, donationsRecieved, donationsGiven } = userData
+
+    // Run assertions for all the user fields and verify correctness
+    assert.equal(user1, userAddress, 'Address of the updated user is incorrect')
+    assert.equal(userName, 'natsu', 'Username of the updated user is incorrect')
+    assert.equal(email, 'anime@cool.com', 'Email of the updated user is incorrect')
+    assert.equal(info, 'I read manga', 'Info of the updated user is incorrect')
+  });
+
+
+
 })
