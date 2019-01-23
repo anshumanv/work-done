5be49b8c240b3cf8ad4fa63b98cf66baca9e3733
Anshuman Verma <anshu.av97@gmail.com>
2019-01-23 19:27:57 +0530

    Remove Stringstore from deploy script

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,7 +1,5 @@
-const StringStore = artifacts.require("StringStore");
 const WorkDone = artifacts.require("WorkDone");
 
 module.exports = function(deployer) {
-  deployer.deploy(StringStore);
   deployer.deploy(WorkDone);
 }
