26182622578cc6da43f0a7e5101c883e49a601ff
Anshuman Verma <anshu.av97@gmail.com>
2019-01-28 11:23:50 +0530

    Add solc merger and fix compiler version

--- contracts/WorkDone.sol
+++ contracts/WorkDone.sol
@@ -1,4 +1,4 @@
-pragma solidity >=0.4.22 <0.6.0;
+pragma solidity 0.5.0;
 
 import "../node_modules/openzeppelin-solidity/contracts/ownership/Ownable.sol";
 import "../node_modules/openzeppelin-solidity/contracts/math/SafeMath.sol";
